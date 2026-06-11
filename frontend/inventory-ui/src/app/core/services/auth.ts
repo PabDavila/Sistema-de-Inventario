70c79b47abf66142ae1bdaf9ed423c6b5dc34ea9
PabDavila <pab.davila@duocuc.cl>
2026-06-11 18:20:38 -0400

    Logica Login

--- frontend/inventory-ui/src/app/core/services/auth.ts
+++ frontend/inventory-ui/src/app/core/services/auth.ts
@@ -1,11 +1,35 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { LoginRequest } from '../../models/login-request';
+import { LoginResponse } from '../../models/login-response';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   private readonly TOKEN_KEY = 'jwt_token';
+
+  private readonly API_URL =
+    'http://localhost:8081/auth';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  login(
+    request: LoginRequest
+  ): Observable<LoginResponse> {
+
+    return this.http.post<LoginResponse>(
+      `${this.API_URL}/login`,
+      request
+    );
+  }
 
   saveToken(token: string): void {
 
--- frontend/inventory-ui/src/app/pages/login/login.ts
+++ frontend/inventory-ui/src/app/pages/login/login.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  username = '';
+  password = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  login(): void {
+
+    this.authService.login({
+
+      username: this.username,
+      password: this.password
+
+    }).subscribe({
+
+      next: (response) => {
+
+        this.authService.saveToken(
+          response.token
+        );
+
+        this.router.navigate([
+          '/dashboard'
+        ]);
+      },
+
+      error: () => {
+
+        alert(
+          'Usuario o contraseña incorrectos'
+        );
+      }
+    });
+  }
 }
